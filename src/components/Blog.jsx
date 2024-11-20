6ca4c60b8ff6040c1c4db7618f4c24c0cb6dcc38
Akash  J <akashjitu18@gmail.com>
2024-11-21 04:26:49 +0530

    Update Blog.jsx

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -5,7 +5,7 @@ const BlogCard = ({ image, category, title, description, profileImage, author, d
         <div className="h-full rounded-lg overflow-hidden">
             <img
                 className="w-[384px] h-[240px] object-cover object-center"
-                src={image}
+                src={"image"}
                 alt="blog"
             />
             <div className="py-2">
